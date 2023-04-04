5757ea346dc9110dfd8929d6387a46827432ee5a
Mouli2000 <moulisitthan10@gmail.com>
2023-04-04 20:01:00 +0530

    Added : Check Two Lines are Equal or Not

--- src/main/java/com/bridgelabz/linecomparison/LineComparison.java
+++ src/main/java/com/bridgelabz/linecomparison/LineComparison.java
@@ -16,7 +16,21 @@ public class LineComparison {
 		System.out.print("Enter Y2 : ");
 		int y2 = scan.nextInt();
 		
-		double length = Math.sqrt((Math.pow((x2-x1), 2)+(Math.pow((y2-y1), 2))));
-		System.out.println("Length of the Line : "+length);
+		System.out.print("Enter P1 : ");
+		int p1 = scan.nextInt();
+		System.out.print("Enter Q1 : ");
+		int q1 = scan.nextInt();
+		System.out.print("Enter P2 : ");
+		int p2 = scan.nextInt();
+		System.out.print("Enter Q2 : ");
+		int q2 = scan.nextInt();
+		
+		double length1 = Math.sqrt((Math.pow((x2-x1), 2)+(Math.pow((y2-y1), 2))));
+		double length2 = Math.sqrt((Math.pow((p2-p1), 2)+(Math.pow((q2-q1), 2))));
+		
+		if(length1 == length2)
+			System.out.println("Two Lines are Equal !!!");
+		else
+			System.out.println("Two Lines are Not Equal...");
 	}
 }
