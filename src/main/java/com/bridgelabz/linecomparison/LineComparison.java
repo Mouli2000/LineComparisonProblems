575161183778e61f847981b0cb901df88e41ab98
Mouli2000 <moulisitthan10@gmail.com>
2023-04-10 12:18:53 +0530

    Added : Compare Two Lines With Equals and CompareTo Methods

--- src/main/java/com/bridgelabz/linecomparison/LineComparison.java
+++ src/main/java/com/bridgelabz/linecomparison/LineComparison.java
@@ -3,6 +3,21 @@ package com.bridgelabz.linecomparison;
 import java.util.Scanner;
 
 public class LineComparison {
+	
+	static void comparison(double length1, double length2) {
+		
+		Double value1 = new Double(length1);
+		Double value2 = new Double(length2);
+		
+		int compare = value1.compareTo(value2);
+		boolean equals = value1.equals(value2);
+		if(equals == true)
+			System.out.println("Two Lines are Equal !!!");
+		else if(compare > 0)
+			System.out.println("Line1 is Greater Than Line2...");
+		else
+			System.out.println("Line1 is Lesser Than Line2...");
+	}
 
 	public static void main(String[] args) {
 		
@@ -28,16 +43,6 @@ public class LineComparison {
 		double length1 = Math.sqrt((Math.pow((x2-x1), 2)+(Math.pow((y2-y1), 2))));
 		double length2 = Math.sqrt((Math.pow((p2-p1), 2)+(Math.pow((q2-q1), 2))));
 		
-		Double value1 = new Double(length1);
-		Double value2 = new Double(length2);
-		
-		int compare = value1.compareTo(value2);
-		
-		if(compare == 0)
-			System.out.println("Two Lines are Equal !!!");
-		else if(compare > 0)
-			System.out.println("Line1 is Greater Than Line2...");
-		else
-			System.out.println("Line1 is Lesser Than Line2...");
+		comparison(length1, length2);
 	}
 }
